c835b43dede3a3d05807bd3e302b6f821623f476
edofazza <fazzari.edoardo@gmail.com>
2021-01-03 13:54:02 +0100

    Mnesia register working

--- ErlangServer/src/mnesiaFunctions.erl
+++ ErlangServer/src/mnesiaFunctions.erl
@@ -10,7 +10,7 @@
 -author("edoardo").
 
 %% API
--export([init/0, login/4, register/4]).
+-export([init/0, login/4, register/4, all_user/0, readTest/1]).
 
 -include_lib("stdlib/include/qlc.hrl").
 -include("headers/records.hrl").
@@ -22,12 +22,12 @@ init() ->
   mnesia:create_schema([node()]),
   mnesia:start(),
   mnesia:create_table(unisup_users,
-    [{attributes, record_info(fields, unisup_users)},
-      {disc_copies, node()},
-      {type, ordered_set}]),
-  mnesia:create_table(unisup_messages, [{attributes, record_info(fields, unisup_messages)},
-    {disc_copies, node()},
-    {type, bag}]).
+    [{attributes, record_info(fields, unisup_users)}]),
+      %{disc_copies, node()},
+      %{type, ordered_set}]),
+  mnesia:create_table(unisup_messages, [{attributes, record_info(fields, unisup_messages)}]).
+    %{disc_copies, node()},
+    %{type, bag}]).
 
 
 %%%===================================================================
@@ -56,12 +56,10 @@ login(Username, Password, NodeName, Pid) ->
         case mnesia:read({unisup_users, Username}) =:= [] of
           true -> % User present
             %% GET PASSWORD
-            %Pass = select(qlc:q([X || X <- mnesia:table(unisup_users),
-             % (X#unisup_users.username == Username)])),
-             [_,Pass,_,_] = [User] = mnesia:read(unisup_users, Username),
-
+            {_, [_, Pass, _, _]} = mnesia:select(unisup_users, [{username = Username, _ = '_', _ = '_', _ = '_'}]),
+            %%Q = qlc:q([{R#unisup_users.username, R#unisup_users.password} || R <- mnesia:table(base)])
             %% CHECK IF THE PASSWORD IS CORRECT
-            case Password == Pass of
+            case [Password] == Pass of
                %% If the password is equal to the one inserted then I update the data in Mnesia
                %% and I return true. Otherwise, I return false
                true ->
@@ -78,12 +76,25 @@ login(Username, Password, NodeName, Pid) ->
 register(Username, Password, NodeName, Pid) ->
   F = fun() ->
         case mnesia:read({unisup_users, Username}) =:= [] of
-          true -> % User present
-            false;
-          false ->
+          true -> % User not present
             add_user(Username, Password, NodeName, Pid),
-            true
+            true;
+          false ->
+            false
         end
     end,
     mnesia:activity(transaction, F).
+
+readTest(Username) ->
+  F = fun() ->
+    mnesia:read({unisup_users, Username})
+      end,
+  mnesia:activity(transaction, F).
+
+all_user() ->
+  F = fun() ->
+    Q = qlc:q([{E#unisup_users.username, E#unisup_users.password, E#unisup_users.nodeName, E#unisup_users.pid} || E <- mnesia:table(unisup_users)]),
+    qlc:e(Q)
+      end,
+  mnesia:transaction(F).
 
