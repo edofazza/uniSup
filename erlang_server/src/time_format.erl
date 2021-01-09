0e3775c4da00ba78eff7299af2104e0fe4d820e8
edofazza <fazzari.edoardo@gmail.com>
2021-01-09 15:51:59 +0100

    time_format

--- erlang_server/src/time_format.erl
+++ erlang_server/src/time_format.erl
@@ -16,7 +16,11 @@ format_utc_timestamp() ->
   TS = {_, _, Micro } = os:timestamp(),
   {{Year, Month, Day}, {Hour, Minute, Second}} = calendar:now_to_universal_time(TS),
   Mstr = element(Month, {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug" ,"Sep", "Oct", "Nov", "Dec"}),
-  erl_types:atom_to_string(Day) ++ " " ++ Mstr ++ " " ++
-  erl_types:atom_to_string(Year) ++ " " ++ erl_types:atom_to_string(Hour) ++ ":" ++
-  erl_types:atom_to_string(Minute) ++ ":" ++ erl_types:atom_to_string(Second) ++ "." ++
-  erl_types:atom_to_string(Micro).
+  %erl_types:atom_to_string(Day) ++ " " ++ Mstr ++ " " ++
+  %erl_types:atom_to_string(Year) ++ " " ++ erl_types:atom_to_string(Hour) ++ ":" ++
+  %erl_types:atom_to_string(Minute) ++ ":" ++ erl_types:atom_to_string(Second) ++ "." ++
+  %erl_types:atom_to_string(Micro).
+  erl_types:atom_to_string(Year) ++ "-" ++ erl_types:atom_to_string(Month) ++ "-" ++
+    erl_types:atom_to_string(Day) ++ "T" ++ erl_types:atom_to_string(Hour) ++ ":" ++
+    erl_types:atom_to_string(Minute) ++ ":" ++ erl_types:atom_to_string(Second) ++ "." ++
+    erl_types:atom_to_string(Micro) ++ "Z".
