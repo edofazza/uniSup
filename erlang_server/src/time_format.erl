d902f1f5d9f8f27c9dc0d8d138188fffe5bb0970
edofazza <fazzari.edoardo@gmail.com>
2021-01-09 16:31:30 +0100

    Update time_format.erl

--- erlang_server/src/time_format.erl
+++ erlang_server/src/time_format.erl
@@ -15,12 +15,41 @@
 format_utc_timestamp() ->
   TS = {_, _, Micro } = os:timestamp(),
   {{Year, Month, Day}, {Hour, Minute, Second}} = calendar:now_to_universal_time(TS),
-  Mstr = element(Month, {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug" ,"Sep", "Oct", "Nov", "Dec"}),
+  %Mstr = element(Month, {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug" ,"Sep", "Oct", "Nov", "Dec"}),
   %erl_types:atom_to_string(Day) ++ " " ++ Mstr ++ " " ++
   %erl_types:atom_to_string(Year) ++ " " ++ erl_types:atom_to_string(Hour) ++ ":" ++
   %erl_types:atom_to_string(Minute) ++ ":" ++ erl_types:atom_to_string(Second) ++ "." ++
   %erl_types:atom_to_string(Micro).
-  erl_types:atom_to_string(Year) ++ "-" ++ erl_types:atom_to_string(Month) ++ "-" ++
-    erl_types:atom_to_string(Day) ++ "T" ++ erl_types:atom_to_string(Hour) ++ ":" ++
-    erl_types:atom_to_string(Minute) ++ ":" ++ erl_types:atom_to_string(Second) ++ "." ++
-    erl_types:atom_to_string(Micro) ++ "Z".
+  StringMonth = case Month < 10 of
+    true ->
+      "0" ++ erl_types:atom_to_string(Month);
+    false ->
+      erl_types:atom_to_string(Month)
+  end,
+  StringDay = case Day < 10 of
+                true ->
+                  "0" ++ erl_types:atom_to_string(Day);
+                false ->
+                  erl_types:atom_to_string(Day)
+              end,
+  StringHour = case Hour < 10 of
+                 true ->
+                   "0" ++ erl_types:atom_to_string(Hour);
+                 false ->
+                   erl_types:atom_to_string(Hour)
+               end,
+  StringMinute = case Minute < 10 of
+                   true ->
+                     "0" ++ erl_types:atom_to_string(Minute);
+                   false ->
+                     erl_types:atom_to_string(Minute)
+                 end,
+  StringSecond = case Second < 10 of
+                   true ->
+                     "0" ++ erl_types:atom_to_string(Second);
+                   false ->
+                     erl_types:atom_to_string(Second)
+                 end,
+  erl_types:atom_to_string(Year) ++ "-" ++ StringMonth ++ "-" ++
+    StringDay ++ "T" ++ StringHour ++ ":" ++ StringMinute ++ ":" ++ StringSecond ++ "." ++
+    string:substr(erl_types:atom_to_string(Micro), 1, 2) ++ "Z".
