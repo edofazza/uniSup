c4ea1937bc1c61bb3c4004ba4c3bce7d81d5235a
Mirco-Ramo <m.ramo@studenti.unipi.it>
2021-01-15 13:28:04 +0100

    test session on rabbitmq

--- erlang_server/starter.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-rem rebar3 compile
-cd ./src
-erlc listener.erl
-erlc mnesiaFunctions.erl
-erlc time_format.erl
-erlc uniSupErlangServer.erl
---
+++ erlang_server/starterNotWorking.bat
@@ -0,0 +1,5 @@
+
+start 
+rebar3 compile
+
+rebar3 shell --sname unisup_server@localhost --setcookie unisup
