ffd0f263fe8013518e8a8f12978bc729967c2f2b
Mirco-Ramo <m.ramo@studenti.unipi.it>
2021-01-17 18:21:52 +0100

    test and corrections,but it still does not work

---
+++ erlang_server/starter.cmd
@@ -0,0 +1,3 @@
+@echo off
+
+rebar3 shell --sname unisup_server@localhost --setcookie unisup --script src/run_listener.escript
--- erlang_server/starterNotWorking.bat
+++
@@ -1,5 +0,0 @@
-
-start 
-rebar3 compile
-
-rebar3 shell --sname unisup_server@localhost --setcookie unisup
