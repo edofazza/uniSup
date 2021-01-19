3dd335738405aa7aedac6dd1075a1e18095e3370
Mirco-Ramo <m.ramo@studenti.unipi.it>
2021-01-19 11:31:37 +0100

    send message controller

--- client/src/main/java/it/unipi/dii/dsmt/unisup/controller/MainFrameController.java
+++ client/src/main/java/it/unipi/dii/dsmt/unisup/controller/MainFrameController.java
@@ -1,18 +1,21 @@
 package it.unipi.dii.dsmt.unisup.controller;
 
 import it.unipi.dii.dsmt.unisup.NewMain;
+import it.unipi.dii.dsmt.unisup.beans.Chat;
+import it.unipi.dii.dsmt.unisup.beans.Message;
+import it.unipi.dii.dsmt.unisup.communication.AuthGateway;
+import it.unipi.dii.dsmt.unisup.communication.Authenticator;
+import it.unipi.dii.dsmt.unisup.communication.MessageGateway;
+import it.unipi.dii.dsmt.unisup.userinterface.CurrentUI;
+import it.unipi.dii.dsmt.unisup.userinterface.javafxextensions.panes.scrollpanes.ChatScrollPane;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
-import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
-import javafx.stage.Popup;
 import javafx.stage.Stage;
 
-import javax.xml.soap.Text;
 import java.io.IOException;
 
 public class MainFrameController {
@@ -29,6 +32,8 @@ public class MainFrameController {
     @FXML
     private TextArea messageTextArea;
 
+    private final int TIMEOUT = 5000;
+
 
     @FXML
     private void initialize() {
@@ -37,6 +42,9 @@ public class MainFrameController {
 
     private void setActionCommands() {
         logoutBtn.setOnAction(e ->{
+            Authenticator au = AuthGateway.getInstance();
+            au.logout(CurrentUI.getUser());
+            CurrentUI.userExit();
                 try {
                     NewMain.getStage().setScene(new Scene(NewMain.loadFXML("LoginFrame")));
                 } catch (IOException ioException) {
@@ -61,7 +69,26 @@ public class MainFrameController {
 
         });
         sendBtn.setOnAction(e ->{
+            String senderUsername = "GOOFIE"; //TODO take the username of the sender (CurrentUI can help you)
+            String receiverUsername = "GOOFIE"; //TODO take the username of the receiver
+            String textMessage = "GOOFIE"; //TODO take the message text from the text area
+            Chat chat = ChatScrollPane.getChat(); //TODO get the actual chat
 
+            if (chat == null)
+                return;
+
+            Message message = new Message(senderUsername, receiverUsername, textMessage);
+            MessageGateway
+                    .getInstance()
+                    .sendMessage(
+                            message,
+                            TIMEOUT
+                    );
+
+            chat.addMessageToHistory(message);
+
+            //TODO empty chat area
+            //TODO visualize the new chat
         });
     }
 }
