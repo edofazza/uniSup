110a1159f3197f8dccd5d1b19d3d0f3653216c8c
Mirco-Ramo <m.ramo@studenti.unipi.it>
2021-01-20 11:01:50 +0100

    chat sorter implemented

--- client/src/main/java/it/unipi/dii/dsmt/unisup/utils/ChatSorter.java
+++ client/src/main/java/it/unipi/dii/dsmt/unisup/utils/ChatSorter.java
@@ -3,16 +3,21 @@ package it.unipi.dii.dsmt.unisup.utils;
 import it.unipi.dii.dsmt.unisup.beans.Chat;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class ChatSorter {
-    private List<Chat> chats= new ArrayList<>();
+    private List<Chat> chats;
 
     public ChatSorter(List<Chat> chats){
         this.chats=chats;
     }
 
     public List<Chat> sort(){
-        return null;
+        if(chats ==null || chats.size()<1)
+            return new ArrayList<>();
+        //chats.sort((o1, o2) -> o1.getHistory().get(o1.getHistory().size()).getTimestamp().compareTo(o2.getHistory().get(o2.getHistory().size()).getTimestamp()));
+        chats.sort(Comparator.comparing(o -> o.getHistory().get(o.getHistory().size()).getTimestamp()));
+        return chats;
     }
 }
