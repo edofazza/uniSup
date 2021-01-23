a23bbe27efa9393f52e1da1125550de6b63e08ce
Mirco-Ramo <m.ramo@studenti.unipi.it>
2021-01-23 18:42:04 +0100

    synchronization of shared data structures in the model

--- client/src/main/java/it/unipi/dii/dsmt/unisup/beans/Chat.java
+++ client/src/main/java/it/unipi/dii/dsmt/unisup/beans/Chat.java
@@ -3,11 +3,12 @@ package it.unipi.dii.dsmt.unisup.beans;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Chat {
     private final String usernameContact;
     private List<Message> history;
-    private int unreadMessages = 0;
+    private AtomicInteger unreadMessages = new AtomicInteger(0);
 
     public Chat(String usernameContact, List<Message> history) {
         this.usernameContact = usernameContact;
@@ -17,7 +18,7 @@ public class Chat {
     public Chat(String usernameContact, List<Message> history, int unreadMessages){
         this.usernameContact=usernameContact;
         this.history= Collections.synchronizedList(history);
-        this.unreadMessages=unreadMessages;
+        this.unreadMessages.getAndSet(unreadMessages);
     }
 
     public Chat(String usernameContact){
@@ -41,18 +42,18 @@ public class Chat {
     }
 
     public int getUnreadMessages() {
-        return unreadMessages;
+        return unreadMessages.get();
     }
 
     public void increaseUnreadMessages(int unreadMessages) {
-        this.unreadMessages += unreadMessages;
+        this.unreadMessages.getAndAdd(unreadMessages);
     }
     public void readAllMessages() {
-        this.unreadMessages = 0;
+        this.unreadMessages .getAndSet(0);
     }
 
     public boolean hasUnreadMessages(){
-        return this.unreadMessages > 0;
+        return this.unreadMessages.get()>0;
     }
     @Override
     public String toString() {
